6b1180130a79c6378df85e29f00b1625efb9ec18
leosole <leosole@gmail.com>
2021-10-13 16:18:49 -0300

    Update apiUser.js

--- front/src/apiUser.js
+++ front/src/apiUser.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:1337/',
+  baseURL: 'https://aglomerados.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json',
   },
