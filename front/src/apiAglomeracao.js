facde48c22ffd876c3575fe431aa7c67075e48cb
leosole <leosole@gmail.com>
2021-10-13 16:58:22 -0300

    Update apiAglomeracao.js

--- front/src/apiAglomeracao.js
+++ front/src/apiAglomeracao.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3030/',
+  baseURL: 'https://aglomerados-aval-back.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json',
   },
